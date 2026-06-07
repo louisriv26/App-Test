567c2f59b4047bd54947283ad79ecc7001e9691f
louisriv26 <louis.rivalland@gmail.com>
2026-06-07 22:41:07 +0400

    v77

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* Stage 5C-R5 service worker — prototype-75 */
-const CACHE_NAME = 'luisa-24h-prototype-75';
+/* Stage 5C-R5 service worker — prototype-77 */
+const CACHE_NAME = 'luisa-24h-prototype-77';
 const APP_SHELL = [
   './',
   './index.html',
